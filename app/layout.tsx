c835410a6c8ea7c338cc06e8ce4004287296261b
Arittro7 <arittrowpdev@gmail.com>
2025-02-22 00:28:20 +0600

    Scroll to top

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Components/Shared/Navbar/Navbar";
 import Footer from "./Components/home/Footer/Footer";
+import ScrollToTop from "./Components/Shared/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
+        <ScrollToTop></ScrollToTop>
       </body>
     </html>
   );
